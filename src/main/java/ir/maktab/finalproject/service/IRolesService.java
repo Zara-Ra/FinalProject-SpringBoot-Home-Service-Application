a2f62c5763007856e4134459083c9520b5063999
zahra.rahimi <zahra.rahimi@gmail.com>
2023-01-30 07:27:58 +0330

    Final Commit of Phase2

--- src/main/java/ir/maktab/finalproject/service/IRolesService.java
+++ src/main/java/ir/maktab/finalproject/service/IRolesService.java
@@ -2,6 +2,8 @@ package ir.maktab.finalproject.service;
 
 public interface IRolesService<T> {
     T signUp(T role);
+
     T signIn(String email, String password);
-    T changePassword(T role,String oldPassword,String newPassword);
+
+    T changePassword(T role, String oldPassword, String newPassword);
 }
--- src/main/java/ir/maktab/finalproject/service/IService.java
+++ src/main/java/ir/maktab/finalproject/service/IService.java
@@ -2,8 +2,10 @@ package ir.maktab.finalproject.service;
 
 import java.util.Optional;
 
-public interface IService<T>{
+public interface IService<T> {
     T add(T service);
+
     void delete(T service);
+
     Optional<T> findByName(String name);
 }
